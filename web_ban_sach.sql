610f0dc36ef0d1e70bc92281cdd626a33a5e630a
duyhocit <hoangduyoffline@gmail.com>
2024-09-25 15:07:45 +0700

    cap nhat database

--- web_ban_sach.sql
+++ web_ban_sach.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 18, 2024 at 12:53 PM
+-- Generation Time: Sep 25, 2024 at 08:07 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -32,6 +32,13 @@ CREATE TABLE `authors` (
   `name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`id`, `name`) VALUES
+(1, 'Spencer Johnson');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +49,13 @@ CREATE TABLE `genres` (
   `id` int NOT NULL,
   `name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(1, 'kinh dị');
 
 -- --------------------------------------------------------
 
@@ -56,12 +70,23 @@ CREATE TABLE `orders` (
   `recipient_email` varchar(255) NOT NULL,
   `recipient_phone` varchar(255) NOT NULL,
   `recipient_address` varchar(255) NOT NULL,
-  `booking_date` date NOT NULL,
+  `order_date` date NOT NULL,
   `total_amount` varchar(255) NOT NULL,
   `note` varchar(255) NOT NULL,
   `payment_method_id` int NOT NULL,
-  `payment_status_id` int NOT NULL
+  `payment_status_id` int NOT NULL,
+  `Order_ID` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `recipient_name`, `recipient_email`, `recipient_phone`, `recipient_address`, `order_date`, `total_amount`, `note`, `payment_method_id`, `payment_status_id`, `Order_ID`) VALUES
+(1, 16, 'hoàng khánh chi', 'khanhchi@gmail.com', '0331254552', 'an thượng hoài đức hà nội', '2024-09-26', '30000', 'giao hàng nhớ gọi điện tôi không muốn có 1 chút sai sót nào trong vận chuyển', 1, 6, 'TH4004'),
+(2, 14, 'hoàng quốc duy', 'hoangduyoffline@gmail.com', '0337035875', 'hà nội', '2024-09-26', '3000', 'không biế nói gì hơn là tuyệt vời', 5, 2, 'TH455'),
+(3, 14, 'd', 'eq', 'ưe', 'e1', '2024-09-20', '31', '312', 2, 5, '1'),
+(4, 13, 'hoàng khánh chi', 'hoangchi@gmail.com', '123', '123', '2024-09-27', 'qư', 'eq', 4, 4, 'e2');
 
 -- --------------------------------------------------------
 
@@ -71,12 +96,20 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `order_details` (
   `id` int NOT NULL,
-  `order_id` int NOT NULL,
+  `orders_id` int NOT NULL,
   `product_id` int NOT NULL,
   `quantity` decimal(10,2) NOT NULL,
-  `unit_price` int NOT NULL,
+  `unit_price` varchar(200) NOT NULL,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `orders_id`, `product_id`, `quantity`, `unit_price`, `price`) VALUES
+(1, 1, 4, '10.00', '200', '121000.00'),
+(2, 1, 4, '10.00', '1222', '121000.00');
 
 -- --------------------------------------------------------
 
@@ -89,6 +122,17 @@ CREATE TABLE `payment_method` (
   `payment_method_name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `payment_method`
+--
+
+INSERT INTO `payment_method` (`id`, `payment_method_name`) VALUES
+(1, 'thanh toán khi nhận hàng'),
+(2, 'Thẻ tín dụng/ghi nợ'),
+(3, 'ví điện tử'),
+(4, 'hệ thống thanh toán trực tuyến'),
+(5, 'thẻ quà tặng');
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +143,18 @@ CREATE TABLE `payment_status` (
   `id` int NOT NULL,
   `payment_status` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `payment_status`
+--
+
+INSERT INTO `payment_status` (`id`, `payment_status`) VALUES
+(1, 'Chờ xác nhận'),
+(2, 'Đang xử lý '),
+(3, 'Đã thanh toán'),
+(4, 'Thất bại'),
+(5, 'Đã hoàn tiền'),
+(6, 'Đã hủy');
 
 -- --------------------------------------------------------
 
@@ -114,8 +170,16 @@ CREATE TABLE `products` (
   `genre_id` int NOT NULL,
   `published_date` date NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `description` varchar(200) NOT NULL
+  `description` varchar(200) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `book_name`, `title`, `author_id`, `genre_id`, `published_date`, `price`, `description`, `image`) VALUES
+(4, 'who moved my cheese', 'Ai Lấy Miếng Pho Mát Của Tôi?', 1, 1, '2024-09-26', '122222.00', 'Ai Lấy Miếng Pho Mát Của Tôi?\" (tựa gốc: Who Moved My Cheese?) là một cuốn sách nổi tiếng của tác giả Spencer Johnson, đã mang đến cho người đọc những bài học quý giá về sự thay đổi trong cuộc sống', './uploads/products/1727184235IMG_0843.png');
 
 -- --------------------------------------------------------
 
@@ -127,6 +191,14 @@ CREATE TABLE `role` (
   `id` int NOT NULL,
   `role` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'user');
 
 -- --------------------------------------------------------
 
@@ -166,8 +238,18 @@ CREATE TABLE `users` (
   `address` varchar(200) DEFAULT NULL,
   `avatar` varchar(200) DEFAULT NULL,
   `nationality` varchar(200) DEFAULT NULL,
-  `id_role` int NOT NULL
+  `id_role` int NOT NULL,
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name_user`, `email`, `phone`, `address`, `avatar`, `nationality`, `id_role`, `password`) VALUES
+(14, 'Hoang duy2', 'hoangduyoffline@gmail.com', '3337035875', 'hà nội', './uploads/procuts1727187811459039876_1203660540836806_8466509052454082988_n.jpg', 'việt nam', 1, 'hoangduy20042'),
+(16, 'duy123456', 'hoangduyoffline@gmail.com', '0904454663', 'hà nọi', NULL, 'việt nam', 2, 'hoangduy2004'),
+(17, 'Hoang duy2004', 'hoangduyoffline23@gmail.com', '3337035875', 'hà nội', './uploads/procuts1727187590IMG_UPLOAD_20240801_193925.jpg', 'việt nam', 1, '1233');
 
 --
 -- Indexes for dumped tables
@@ -199,8 +281,8 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `order_details`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `LK` (`order_id`),
-  ADD KEY `KN` (`product_id`);
+  ADD KEY `KN` (`product_id`),
+  ADD KEY `Lien_ket102` (`orders_id`);
 
 --
 -- Indexes for table `payment_method`
@@ -257,49 +339,49 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `payment_method`
 --
 ALTER TABLE `payment_method`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `payment_status`
 --
 ALTER TABLE `payment_status`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `shopping_cart_details`
@@ -311,7 +393,7 @@ ALTER TABLE `shopping_cart_details`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
@@ -322,15 +404,14 @@ ALTER TABLE `users`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `Lien_ket` FOREIGN KEY (`payment_status_id`) REFERENCES `payment_status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `Lien_ket2` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `Lien_ket3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `Lien_ket2` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `order_details`
 --
 ALTER TABLE `order_details`
   ADD CONSTRAINT `KN` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `LK` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `Lien_ket102` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `products`
@@ -338,12 +419,6 @@ ALTER TABLE `order_details`
 ALTER TABLE `products`
   ADD CONSTRAINT `lien_ket4` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `lien_ket5` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `shopping_cart`
---
-ALTER TABLE `shopping_cart`
-  ADD CONSTRAINT `Lien_ket8` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `shopping_cart_details`
