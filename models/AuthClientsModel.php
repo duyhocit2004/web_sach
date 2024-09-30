bd6eaa1283fd4a40603a018b36364a2fb8e018dc
duyhocit <hoangduyoffline@gmail.com>
2024-10-01 03:01:23 +0700

    lam login vs register vs logout user

--- models/AuthClientsModel.php
+++ models/AuthClientsModel.php
@@ -1,4 +1,46 @@
 <?php 
 class AuthClientsModel{
-   
+   public $data ;
+   public function __construct(){
+    $this->data = connectDB();
+   }
+    public function checklogin($email,$password){
+        $sql = 'SELECT * FROM users WHERE email = :email';
+        $stmt = $this->data->prepare($sql);
+        $stmt ->execute(
+        [
+            ':email' => $email,
+
+        ]
+        );
+        $user = $stmt -> fetch();
+        if($user && $password == $user['password']){
+            if($user['id_role'] == 2){
+                $_SESSION['user_clients'] =[
+                    'email' => $user['email'],
+                    'password' => $user['password'],
+                ];
+                // echo ($user['email']);die();
+                return true;
+            }else{
+                header('Location : ' . BASE_URL);
+                exit();
+            }
+        }else{
+            // header('Location : ' . BASE_URL .'?act=login');
+            return "<script>alert('Tên đăng nhập hoặc mật khẩu không đúng. Vui lòng nhập lại.');</script>";
+        }
+
+    }
+    public function AddCount($name_user,$email,$password){
+        $sql = "INSERT INTO users (name_user, email , password , id_role) VALUES( :name_user , :email , :password , :role) ";
+        $stmt = $this->data->prepare($sql);
+        $stmt -> execute([
+            ':name_user' =>$name_user,
+            ':email' => $email,
+            ':password' => $password,
+            ':role' => 2,
+        ]);
+        return true;
+        }
 }
--- view/auth/register.php
+++ view/auth/register.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iofrm</title>
+    <title>đăng kí</title>
     <link rel="stylesheet" type="text/css" href="assets/login/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/login/css/fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/login/css/iofrm-style.css">
@@ -20,22 +20,17 @@
             <div class="form-holder">
                 <div class="form-content">
                     <div class="form-items">
-                        <div class="website-logo-inside logo-normal">
-                            <a href="index.html">
-                                <div class="logo">
-                                    <img class="logo-size" src="assets/login/images/logo-pink.svg" alt="">
-                                </div>
-                            </a>
+                    <div class="website-logo-inside logo-normal">
+                            <img class="logo-size" height="100px" style="width: 300px;" src="uploads/clients/logo/logo.png" alt="">                        
                         </div>
-                        <h3 class="font-md">Get more things done with Loggin platform.</h3>
-                        <p>Access to the most powerfull tool in the entire design and web industry.</p>
-                        <form>
-                            <input class="form-control" type="text" name="name" placeholder="Full Name" required>
+                        <h3 class="font-md">đăng kí tài khoản</h3>
+                        <form action ="<?= BASE_URL . '?act=post-register' ?>" method="POST">
+                            <input class="form-control" type="name_user" name="name_user" placeholder="Full Name" required>
                             <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
                             <input class="form-control" type="password" name="password" placeholder="Password" required>
                             <div class="form-button d-flex">
-                                <button id="submit" type="submit" class="btn btn-primary">Register</button>
-                                <a href="?act=login" class="btn btn-outline-primary">Login</a>
+                                <button id="submit" type="submit" class="btn btn-primary">đăng kí</button>
+                                <a href="?act=login" class="btn btn-outline-primary">đăng nhập</a>
                             </div>
                         </form>
                         <div class="other-links">
